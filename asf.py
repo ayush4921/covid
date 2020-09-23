34a884fcdd21047fd9114587f720673e00c023e4
Jon <jambler24@gmail.com>
2020-09-23 10:56:55 +0200

    More explicit paths for libs

--- asf.py
+++ asf.py
@@ -1,23 +1,25 @@
  # to predict assuming the file path to the image.png is
-! pip install keras
 import keras
+from keras.models import model_from_json
+from keras.preprocessing.image import img_to_array
+import cv2
+import numpy as np
+
 def load_model():
     # load json and create model
-    json_file = open('../input/hellomanasn/model.json', 'r')
+    json_file = open('./model.json', 'r')
     loaded_model_json = json_file.read()
     json_file.close()
     loaded_model = model_from_json(loaded_model_json)
     # load weights into new model
-    loaded_model.load_weights("../input/hellomanasn/model.h5")
+    loaded_model.load_weights("./model.h5")
     # load training history
     #json_file = open('history.json', 'r')
     #loaded_history = json_file.read()
     #json_file.close()
     print("Loaded model from disk") 
-    return loaded_model, history
-    
- # to load pre-saved model
- (loaded_model, history) = load_model()
+    return loaded_model
+
 def predict_pathogen(img, loaded_model):
     image = cv2.imread(img)
     image = cv2.resize(image, (128, 128), interpolation=cv2.INTER_CUBIC)
@@ -27,6 +29,9 @@ def predict_pathogen(img, loaded_model):
     outcome = loaded_model.predict(image)
     print(outcome)
     return outcome
-    
+
 # test
-pathogen = predict_pathogen("../input/nnnnkln/beach1.bmp", loaded_model)
+# to load pre-saved model
+loaded_model = load_model()
+
+pathogen = predict_pathogen("./beach1.bmp", loaded_model)
